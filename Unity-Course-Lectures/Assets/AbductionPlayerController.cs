848b68b5d72cbe211dce5010242c8316ef6746c5
francescoS <francesco.strada11@gmail.com>
2018-11-16 07:30:24 +0100

    missing scipt

---
+++ Unity-Course-Lectures/Assets/AbductionPlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbductionPlayerController : MonoBehaviour
+{
+    public enum PlayerType { Player0, Player1};
+
+    public PlayerType player;
+    public float movSpeed = 5;
+
+    private KeyCode up, down, left, right;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        up = player == PlayerType.Player0 ? KeyCode.W : KeyCode.UpArrow;
+        down = player == PlayerType.Player0 ? KeyCode.S : KeyCode.DownArrow;
+        left = player == PlayerType.Player0 ? KeyCode.A : KeyCode.LeftArrow;
+        right = player == PlayerType.Player0 ? KeyCode.D : KeyCode.RightArrow;
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKey(up))
+            transform.Translate(transform.forward * movSpeed * Time.deltaTime);
+
+        if (Input.GetKey(down))
+            transform.Translate(-transform.forward * movSpeed * Time.deltaTime);
+
+        if (Input.GetKey(left))
+            transform.Translate(-transform.right * movSpeed * Time.deltaTime);
+
+        if (Input.GetKey(right))
+            transform.Translate(transform.right * movSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Enemy e = other.transform.GetComponent<Enemy>();
+        if (e != null)
+            e.Abduct();
+    }
+}
