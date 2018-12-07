8463c8571215890efc2f44309328c8b21ecbb65a
francescoS <francesco.strada11@gmail.com>
2018-12-07 06:59:04 +0100

    added few assets (post processing, sci-fi scene, temple scene, hdr skybox) + graphics lecture scenes

---
+++ Unity-Course-Lectures/Assets/Scripts/MouseMovingBall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MouseMovingBall : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private LayerMask _groundMask;
+
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, _groundMask))
+        {
+
+
+            //transform.position = new Vector3(hit.point.x, transform.position.y,hit.point.z);
+            _rigidbody.MovePosition(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+
+        }
+        
+    }
+}
---
+++ Unity-Course-Lectures/Assets/Scripts/SciFiOpenDoor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SciFiOpenDoor : MonoBehaviour {
+
+    [SerializeField] private Animator _doorAnimator;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (_doorAnimator == null)
+            return;
+
+        if(other.CompareTag("Player"))
+            _doorAnimator.SetBool("character_nearby", true);
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (_doorAnimator == null)
+            return;
+
+        if (other.CompareTag("Player"))
+            _doorAnimator.SetBool("character_nearby", false);
+    }
+
+
+}
