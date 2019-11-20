778a8f79bc33980ffb68d911fbaf878e789de171
francescoStrada11 <francesco.strada11@gmail.com>
2019-11-20 02:14:28 +0100

    physics folder created + rigidbodies scene

---
+++ Unity-Course-Lectures/Assets/_Physics/Scripts/MoveUpKinematic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUpKinematic : MonoBehaviour
+{
+    private Vector3 originalPosition;
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPosition = transform.position;
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (rigidbody != null)
+                rigidbody.isKinematic = true;
+
+            transform.position = Vector3.MoveTowards(transform.position, originalPosition, 2f * Time.deltaTime);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+            if (rigidbody != null)
+                rigidbody.isKinematic = false;
+    }
+}
---
+++ Unity-Course-Lectures/Assets/_Physics/Scripts/RigidbodyReposition.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyReposition : MonoBehaviour
+{
+    public float simulationTime = 3f;
+    private Vector3 originalPosition;
+    private Rigidbody rigidbody;
+    private bool isKinematicOriginalState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPosition = transform.position;
+        rigidbody = GetComponent<Rigidbody>();
+
+        if (rigidbody != null)
+            isKinematicOriginalState = rigidbody.isKinematic;
+
+        InvokeRepeating("RepositionRigibody", 0f, simulationTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void RepositionRigibody()
+    {
+        if(rigidbody != null)
+            rigidbody.isKinematic = true;
+
+        transform.position = originalPosition;
+
+        if (rigidbody != null)
+            rigidbody.isKinematic = isKinematicOriginalState;
+        
+    }
+}
