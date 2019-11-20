e8e20c5e913bb68ab7dd6ff069937ce3820cb5b0
francescoStrada11 <francesco.strada11@gmail.com>
2019-11-20 03:28:41 +0100

    collider scene ready

--- Unity-Course-Lectures/Assets/_Physics/Scripts/RigidbodyReposition.cs
+++ Unity-Course-Lectures/Assets/_Physics/Scripts/RigidbodyReposition.cs
@@ -6,12 +6,15 @@ public class RigidbodyReposition : MonoBehaviour
 {
     public float simulationTime = 3f;
     private Vector3 originalPosition;
+    private Quaternion originalRotation;
     private Rigidbody rigidbody;
     private bool isKinematicOriginalState;
     // Start is called before the first frame update
     void Start()
     {
         originalPosition = transform.position;
+        originalRotation = transform.rotation;
+
         rigidbody = GetComponent<Rigidbody>();
 
         if (rigidbody != null)
@@ -32,6 +35,7 @@ public class RigidbodyReposition : MonoBehaviour
             rigidbody.isKinematic = true;
 
         transform.position = originalPosition;
+        transform.rotation = originalRotation;
 
         if (rigidbody != null)
             rigidbody.isKinematic = isKinematicOriginalState;
