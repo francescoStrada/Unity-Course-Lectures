8385b78316f7e1fff2b742196b2b63f2a17e23d9
francescoStrada11 <francesco.strada11@gmail.com>
2019-12-04 08:50:04 +0100

    dotween animations

---
+++ Unity-Course-Lectures/Assets/_Animations/Scripts/MoveAlongPath.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MoveAlongPath : MonoBehaviour
+{
+    [SerializeField] private Transform _waypointsRoot;
+    [SerializeField] private float _pathDuration;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_waypointsRoot != null && _waypointsRoot.childCount > 0)
+        {
+            Vector3[] pathPositions = new Vector3[_waypointsRoot.childCount];
+            for (int i = 0; i < _waypointsRoot.childCount; i++)
+                pathPositions[i] = _waypointsRoot.GetChild(i).position;
+
+            transform.DOPath(pathPositions, _pathDuration, PathType.Linear, PathMode.Full3D, resolution: 20, Color.yellow)
+                .SetLookAt(0.01f)
+                .SetLoops(-1, LoopType.Yoyo);
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity-Course-Lectures/Assets/_Animations/Scripts/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DORotate(new Vector3(0f,20f,0f), 1f, RotateMode.Fast).SetLoops(-1, LoopType.Incremental);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
