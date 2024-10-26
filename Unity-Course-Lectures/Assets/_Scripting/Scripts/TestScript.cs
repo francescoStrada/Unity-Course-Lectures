7e6f3f7f1c6e7a7f17fc16f1a62818580c7fb03a
FrancescoRisso <francesco@frisso.net>
2024-10-26 09:58:19 +0200

    Created folder for exercises

---
+++ Unity-Course-Lectures/Assets/_Scripting/Scripts/TestScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("STARTED");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update");
+    }
+}
