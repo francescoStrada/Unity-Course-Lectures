7b060ed0df1c9ee5b3a228253c967e4025bdd430
FrancescoRisso <francesco@frisso.net>
2024-11-21 17:03:07 +0100

    Adding professor's package

---
+++ Consegne/Consegne-vr/Assets/C03-Interactions/C03-Interactions-326834-Risso/ShootingTower.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace c03.exercise
+{
+	public class ShootingTower : MonoBehaviour
+	{
+		//Define here all the variables you will be needing in the script and also all the objects you with to set a reference via the inspector
+		public GameObject bullet;
+		public GameObject target;
+		void Start()
+		{
+			//Retrieve here all the references to other object you will need in the script.
+		}
+
+		void Update()
+		{
+			// Constantly Rotate Tower if Target is NOT in Sight
+			//You can use the function transform.Rotate(): https://docs.unity3d.com/ScriptReference/Transform.Rotate.html
+
+			//Check if Target is visible to the tower
+			if (IsTargetVisible(/* Some varialbes...*/))
+			{
+				PointTarget(/* Some varialbes...*/);
+				
+				//Start Shooting, if already started Shooting don't invoke again
+				//Have a peek to the way we manage an automatic repeated function call in the ShootingTower.cs script. 
+				//As a suggestion, we use the function InvokeRepeating(): https://docs.unity3d.com/ScriptReference/MonoBehaviour.InvokeRepeating.html
+				Shoot();
+
+				return;
+			}
+			
+		}
+
+		private void Shoot()
+		{
+			//Find a creative way to display the shooting behaviour
+		}
+
+		private bool IsTargetVisible(/* Some varialbes...*/)
+		{
+			//In this function you need to check if the target is visible to the tower. This is achieved by checking the three below conditions
+			
+			//CHECK IF IS WITHIN VIEW DISTANCE
+			
+			//CHECK IF FALLS WITHIN VIEW ANGLE
+				
+			//CHECK IF THERE ARE NO OBSTACLES
+					
+			return true;
+		}
+
+		private void PointTarget(/* Some varialbes...*/)
+		{
+			//Rotate the tower in order to always face the target
+		}
+	}
+}
+
---
+++ Consegne/Consegne-vr/Assets/C03-Interactions/C03-Interactions-326834-Risso/SimpleThirdPRigidbodyController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace c03.exercise
+{
+	public class SimpleThirdPRigidbodyController : MonoBehaviour
+	{
+		[SerializeField] private Transform _cameraT;
+		[SerializeField] private float _speed = 5f;
+		[SerializeField] private float _rotationSpeed = 3f;
+
+		private Rigidbody _rigidbody;
+		private Vector3 _inputVector;
+		private float _inputSpeed;
+		private Vector3 _targetDirection;
+		
+		void Start()
+		{
+			_rigidbody = GetComponent<Rigidbody>();
+			_rigidbody.maxDepenetrationVelocity = _speed;
+		}
+
+		void Update()
+		{
+			//Handle the Input
+			float h = Input.GetAxis("Horizontal");
+			float v = Input.GetAxis("Vertical");
+			_inputVector = new Vector3(h, 0, v);
+			_inputSpeed = Mathf.Clamp(_inputVector.magnitude, 0f, 1f);
+
+			//Compute direction According to Camera Orientation
+			_targetDirection = _cameraT.TransformDirection(_inputVector).normalized;
+			_targetDirection.y = 0f;
+		}
+
+		private void FixedUpdate()
+		{
+			Vector3 newDir = Vector3.RotateTowards(transform.forward, _targetDirection, _rotationSpeed * Time.fixedDeltaTime, 0f);
+			
+			Debug.DrawRay(transform.position + transform.up * 3f, _targetDirection * 5f, Color.red);
+			Debug.DrawRay(transform.position + transform.up * 3f, newDir * 5f, Color.blue);
+
+			_rigidbody.MoveRotation(Quaternion.LookRotation(newDir));
+			_rigidbody.MovePosition(_rigidbody.position + transform.forward * _inputSpeed * _speed * Time.fixedDeltaTime);
+		}
+		
+	}
+    
+}
